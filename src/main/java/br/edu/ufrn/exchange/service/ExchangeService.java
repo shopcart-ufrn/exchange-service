63f50c67005ff742efe12778919f6d412a1f00e6
Rary Coringa <rary.goncalves.123@ufrn.edu.br>
2024-12-18 22:38:15 -0300

    feat: remove external request to ensure performance on endpoint response

--- src/main/java/br/edu/ufrn/exchange/service/ExchangeService.java
+++ src/main/java/br/edu/ufrn/exchange/service/ExchangeService.java
@@ -2,42 +2,44 @@ package br.edu.ufrn.exchange.service;
 
 import java.util.Random;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import br.edu.ufrn.exchange.client.FreeCurrencyAPI;
 import br.edu.ufrn.exchange.dto.ExchangeResponseDTO;
 
 @Service
 public class ExchangeService {
 
-    @Autowired
-    private FreeCurrencyAPI freeCurrencyAPI;
-
     private final Random random = new Random();
 
-    public void stop(Integer exitCode) {
+    private void stop(Integer exitCode) {
         Runtime.getRuntime().halt(exitCode);
     }
 
-    public ExchangeResponseDTO getExchange() {
+    private void decideIfStops() {
         Integer randomInt = random.nextInt(1, 11);
 
         if (randomInt == 1) {
             this.stop(randomInt);
         }
+    }
+
+    private Double getRandomRate(Double from, Double to) {
+        return from + (to - from) * Math.random();
+    }
+
+    public ExchangeResponseDTO getExchange() {
+        this.decideIfStops();
 
         String currency = "BRL";
 
-        Double rate;
-
-        try {
-            rate = freeCurrencyAPI.getCurrentRate(currency);
-        } catch (RuntimeException e) {
-            rate = Double.valueOf(6.1);
-        }
+        Double rate = this.getRandomRate(
+            Double.valueOf(5.0),
+            Double.valueOf(7.0)
+            );
     
         return new ExchangeResponseDTO(currency, rate);
     }
+
+
 
 }
